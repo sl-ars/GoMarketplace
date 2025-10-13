08fefada3764c9291cbe8c3eb0fd45b16bf7b1da
NurtileuRakhat <raaaaahaaat@gmail.com>
2025-10-13 12:57:17 +0500

    feat(jwt): add roles

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,13 +1,15 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
 
-func GenerateAccessToken(userID int64, secret []byte) (string, error) {
+func GenerateAccessToken(userID int64, role string, secret []byte) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
+		"role":    role,
 		"exp":     time.Now().Add(15 * time.Minute).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -24,7 +26,15 @@ func GenerateRefreshToken(userID int64, secret []byte) (string, error) {
 }
 
 func ParseToken(tokenStr string, secret []byte) (*jwt.Token, error) {
-	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
 		return secret, nil
 	})
+
+	if err != nil {
+		fmt.Println("JWT parse error:", err)
+	}
+	return token, err
 }
